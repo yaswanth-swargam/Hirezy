779ad19fe64af6573520448ef5968161e68bb5f6
Swargam Yaswanth <223908837+yaswanth-swargam@users.noreply.github.com>
2026-06-10 10:03:43 +0530

    update axios baseURL to production backend

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: 'https://hirezy-production.up.railway.app/api',
   withCredentials: true  // sends cookie automatically with every request
 });
 
